f977949dbabff25fecaf53201d8b25073957ce43
Jonathan <jon_oliveira@outlook.com>
2019-08-20 21:39:25 -0300

    Projeto criado

---
+++ src/main/java/com/nelioalves/cursomc/CursomcApplication.java
@@ -0,0 +1,15 @@
+package com.nelioalves.cursomc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.nelioalves.cursomc.model.Pessoa;
+
+@SpringBootApplication
+public class CursomcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursomcApplication.class, args);
+	}
+	
+}
