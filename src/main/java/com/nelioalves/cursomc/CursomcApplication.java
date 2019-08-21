bf201e20344fdf5e58903e7bdd356d655ae2ae81
Jonathan <jon_oliveira@outlook.com>
2019-08-20 22:05:05 -0300

    REST testado

--- src/main/java/com/nelioalves/cursomc/CursomcApplication.java
+++ src/main/java/com/nelioalves/cursomc/CursomcApplication.java
@@ -3,8 +3,6 @@ package com.nelioalves.cursomc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.nelioalves.cursomc.model.Pessoa;
-
 @SpringBootApplication
 public class CursomcApplication {
 
